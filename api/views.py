78bff378a341785eb366ea795f03a7895888979b
Arman <arman.akbari@gmail.com>
2017-08-31 02:43:43 +0430

    CONNECT api to elastic search

--- api/views.py
+++ api/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from api.serializers import ProductSerializer
 from inventory.models import Product
-
+from elasticsearch import Elasticsearch
+from django.http import JsonResponse
 
 def index(request):
     return render(request, 'api/index.html', {})
@@ -12,9 +13,12 @@ def index(request):
 
 class ProductList(APIView):
 
-    renderer_classes = (JSONRenderer,)
+    # renderer_classes = (JSONRenderer,)
 
     def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        # products = Product.objects.all()
+        # serializer = ProductSerializer(products, many=True)
+        # return Response(serializer.data)
+        es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+        data = es.search(index='test2', doc_type='product')
+        return JsonResponse([hit['_source'] for hit in data['hits']['hits']], safe=False) #[hit['source'] for hit in data['_hits']['hits']])
