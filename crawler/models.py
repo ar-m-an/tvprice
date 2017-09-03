50504678593f6c7c73345fd51a3fd88574145e01
Arman <arman.akbari@gmail.com>
2017-09-03 06:07:41 +0430

    ADD Crawler Models

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    crawler_class = models.CharField(max_length=50)
+    start_url = models.URLField(max_length=256)
+
+
+class ListPage(models.Model):
+    vendor = models.ForeignKey(Vendor)
+    url = models.URLField(max_length=512, unique=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.IntegerField()
+    vendor = models.ForeignKey(Vendor)
+    url = models.URLField(max_length=512, unique=True)
+    image = models.URLField(max_length=512)
+    spec = JSONField()
+
+    def elastic_index(self, connection, index_name):
+        data = {
+            'title': self.title,
+            'price': self.price,
+            'vendor': self.vendor.name,
+            'url': self.url,
+            'image': self.image
+        }
+        data.update(self.spec)
+        try:
+            connection.index(index=index_name, doc_type='product', id=self.id, body=data)
+        except:
+            print('UNABLE INDEXING PRODUCT WITH ID %d ON ELASTICSEARCH.' % self.id)
+
+
+
